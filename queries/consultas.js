1b2a530abfd9e58244b2b9bf8764b6e29fa542b5
O-marin <omarancb@gmail.com>
2024-04-10 11:34:31 -0400

    se agrega consulta getRutinas, y una ruta respectiva

--- queries/consultas.js
+++ queries/consultas.js
@@ -1,1 +1,30 @@
 import pool from '../config/db.js'
+
+//query para crear rutinas
+const agregarRutinas = async ()=>{
+    try{
+        const consultaRutinas = {
+            text: '',
+            values: '',
+        }
+    }catch(err){
+        console.log(err)
+    }
+}
+
+//query para obtener registros
+const getRutinas = async ()=>{
+    try{
+        const consultaRutinas = {
+            text:'select * from ejercicios;'
+        }
+        const result = await pool.query(consultaRutinas);
+        console.log(result.rows);
+        return result.rows;
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+export {getRutinas, agregarRutinas};
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import { getRutinas, agregarRutinas } from '../queries/consultas.js';
 
 const router = express.Router();
 const __dirname = import.meta.dirname
@@ -7,6 +8,11 @@ const __dirname = import.meta.dirname
 
 router.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'../views/index.html'))
+})
+
+router.get('/rutinas', async (req,res)=>{
+    const mostrarRutinas = await getRutinas();
+    res.json(mostrarRutinas);
 })
 
 export default router;
